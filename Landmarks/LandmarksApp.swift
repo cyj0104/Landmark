d769c96a29083411d61d4d1593c6730f84f8049f
최영준/QA1팀/EP <youngjun@nhnenter.com>
2024-10-17 02:52:39 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by NHN on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
